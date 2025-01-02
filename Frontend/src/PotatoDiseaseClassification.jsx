91c54f621193f24b42b33502289563b5870a38eb
HuzaifaWatto <98208958+HuzaifaWatto@users.noreply.github.com>
2025-01-02 18:08:30 +0500

    .

--- Frontend/src/PotatoDiseaseClassification.jsx
+++ Frontend/src/PotatoDiseaseClassification.jsx
@@ -23,7 +23,7 @@ function PotatoDiseaseClassification() {
     formData.append("file", image);
 
     try {
-      const response = await fetch("http://localhost:8000/predict", {
+      const response = await fetch("https://potato-disease-classification-jbau.onrender.com/predict", {
         method: "POST",
         body: formData,
       });
